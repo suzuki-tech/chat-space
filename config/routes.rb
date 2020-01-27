43b0e0baf67cc05759972193fa591f8c0c28c0df
suzuki-tech <nao@store-tech.jp>
2020-01-27 17:02:45 +0900

    edit-routes.rb
    ・ルートパスをサイドバーのみが表示されるように設定
    ・indexアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root "messages#index"
+  root "groups#index"
   # resources :messages, only: :index 
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
