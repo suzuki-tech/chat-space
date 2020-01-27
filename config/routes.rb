2a7b68033fa8d651a4849ec8d30c7189cb070497
suzuki-tech <nao@store-tech.jp>
2020-01-27 17:08:29 +0900

    Update routes.rb
    ・ルートパスをサイドバーのみが表示されるように設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root "messages#index"
+  root "groups#index"
   # resources :messages, only: :index 
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update]
