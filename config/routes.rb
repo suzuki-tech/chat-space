f37d9325e4e59b453e84d7a32d1b659c01c0100b
suzuki-tech <nao@store-tech.jp>
2020-01-27 17:19:38 +0900

    add index-action to group-controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root "groups#index"
   # resources :messages, only: :index 
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
