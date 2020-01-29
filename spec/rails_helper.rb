e61cf25236c012725e746f68b8c94bb5db2d4e47
suzuki-tech <nao@store-tech.jp>
2020-01-29 16:18:45 +0900

    Update rails_helper.rb
    ファクトリーボットとフェイカーを組み合わせる

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,9 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  # FactoryBotとfakerを組み合わせる
+  config.include FactoryBot::Syntax::Methods
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
