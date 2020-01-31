98f924a57937e5777116c07e2a152db967724442
suzuki-tech <nao@store-tech.jp>
2020-01-31 20:16:27 +0900

    Update message.js
    返ってきたJSONをdoneメソッドで受取り、HTMLを作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,46 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+      `<div class="chat-main__message" data-message-id=${message.id}>
+        <div class="chat-main__message__upper-info">
+          <div class="chat-main__message__list__upper-info__talker">
+            ${message.user_name}
+          </div>
+          <div class="chat-main__message__list__upper-info__date">
+          ${message.created_at}
+          </div>
+        </div>
+        <div class="chat-main__message__lower">
+          <p class="chat-main__message__lower__content">
+            ${message.content}
+          </p>
+        </div>
+        <img src=${message.image} >
+      </div>`
+      return html;
+    } else {
+      var html =
+      `<div class="chat-main__message" data-message-id=${message.id}>
+        <div class="chat-main__message__upper-info">
+          <div class="chat-main__message__list__upper-info__talker">
+            ${message.user_name}
+          </div>
+          <div class="chat-main__message__list__upper-info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="chat-main__message__lower">
+          <p class="chat-main__message__lower__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +53,9 @@ $(function(){
       processData: false,
       contentType: false
     })
+      .done(function(data){
+        var html = buildHTML(data);
+        console.log(data)
+      })
   });
 });
